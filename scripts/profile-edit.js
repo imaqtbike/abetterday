26af115a2301bd2e94f9a444d663390612128f63
C <cuong.nguyenmanh@eightcap.com>
2021-11-29 20:56:40 +0700

    ha

--- scripts/profile-edit.js
+++ scripts/profile-edit.js
@@ -1,42 +1,12 @@
-const firstName = document.querySelector('.name_placeholder');
-const lastName = document.querySelector('.last__name_placeholder');
-const email = document.querySelector('.email_placeholder')
-const btnSave = document.querySelector('.profile-button');
-
-window.onload = function () {
-    const lastNameOnLoad = document.querySelector('.last__name_placeholder');
-    const name = window.localStorage.getItem('name');
-    console.log(name)
-    const lastName = window.localStorage.getItem('last_name');
-    console.log(lastName)
-    if (name || lastName) {
-        firstName.innerHTML = name;
-        lastNameOnLoad.innerHTML = lastName;
-    }
+const btnSave = document.querySelector('.profile-button')
+function saveProfileName() {
+    const valOfFname = document.querySelector('.fname__input').value;
+    const valOfSname = document.querySelector('.sname__input').value
+    localStorage.setItem('name', valOfFname)
+    localStorage.setItem('surname', valOfSname)
 }
 
 
-
 btnSave.addEventListener('click', () => {
-    const fNameInputVal = document.querySelector('.fname__input').value;
-    const sNameInputVal = document.querySelector('.sname__input').value;
-    const emailInputVal = document.querySelector('.email__input').value;
-    if (fNameInputVal) {
-        firstName.innerHTML = fNameInputVal
-        window.localStorage.setItem('name', fNameInputVal)
-    }
-    if (sNameInputVal) {
-        lastName.innerHTML = sNameInputVal
-        window.localStorage.setItem('last_name', sNameInputVal)
-    }
-    if (emailInputVal) {
-        email.innerHTML = emailInputVal;
-    }
-    if (fNameInputVal && sNameInputVal && emailInputVal) {
-        firstName.innerHTML = fNameInputVal;
-        lastName.innerHTML = sNameInputVal;
-        email.innerHTML = emailInputVal;
-        window.localStorage.setItem('name', fNameInputVal)
-        window.localStorage.setItem('last_name', sNameInputVal)
-    }
-} )
+    saveProfileName();
+})
